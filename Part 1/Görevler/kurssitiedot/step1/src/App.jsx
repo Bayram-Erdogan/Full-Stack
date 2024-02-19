5d8b92f616f2dbb4dd3fc8cbc57379ad52d80735
Bayram-Erdogan <bayramerdogan.fi@gmail.com>
2024-02-19 08:25:23 +0200

    Part 1 Kurssitiedot step 1

--- Part 1/Görevler/kurssitiedot/step1/src/App.jsx
+++ Part 1/Görevler/kurssitiedot/step1/src/App.jsx
@@ -1,3 +1,65 @@
+/*
+  1.1: kurssitiedot, step1
+
+  A) Luo Vitellä uusi sovellus. Muuta main.jsx muotoon
+
+      import ReactDOM from 'react-dom/client'
+      import App from './App'
+      ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+
+  B) ja tiedosto App.jsx muotoon
+
+      const App = () => {
+        const course = 'Half Stack application development'
+        const part1 = 'Fundamentals of React'
+        const exercises1 = 10
+        const part2 = 'Using props to pass data'
+        const exercises2 = 7
+        const part3 = 'State of a component'
+        const exercises3 = 14
+
+        return (
+          <div>
+            <h1>{course}</h1>
+            <p>
+              {part1} {exercises1}
+            </p>
+            <p>
+              {part2} {exercises2}
+            </p>
+            <p>
+              {part3} {exercises3}
+            </p>
+            <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+          </div>
+        )
+      }
+
+      export default App
+
+   ja poista ylimääräiset tiedostot App.css ja index.css ja hakemisto assets.
+
+  C) Koko sovellus on nyt ikävästi yhdessä komponentissa. Refaktoroi sovelluksen koodi siten, että se koostuu
+  kolmesta uudesta komponentista: Header, Content ja Total. Kaikki data pidetään edelleen komponentissa App,
+  joka välittää tarpeelliset tiedot kullekin komponentille props:ien avulla. Header huolehtii kurssin nimen
+  renderöimisestä, Content osista ja niiden tehtävämääristä ja Total tehtävien yhteismäärästä.
+
+  Tee uudet komponentit tiedostoon App.jsx.
+
+  Komponentin App runko tulee olemaan suunnilleen seuraavanlainen:
+
+    const App = () => {
+      // const-määrittelyt
+
+      return (
+        <div>
+          <Header course={course} />
+          <Content ... />
+          <Total ... />
+        </div>
+      )
+    }
+*/
 const App = () => {
 
   const course = 'Half Stack application development'
@@ -7,7 +69,11 @@ const App = () => {
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
-
+/*
+  Kutsuin alla luomiani komponentteja App-komponentista, joka on pääkomponentti. Tällä tavalla sovelluskomponentti
+  pystyi kommunikoimaan muiden komponenttien kanssa. Kolmen luodun komponentin ansiosta App-komponentti on
+  yksinkertaistunut ja koodin luettavuus on helpottunut.
+*/
   return (
     <div>
       <Header course={course}/>
@@ -25,6 +91,10 @@ const App = () => {
   )
 }
 
+/*
+  Loin 3 komponenttia, nimeltään Content, Header ja Total. Käytin App-komponentin muuttujia parametreina näissä
+  luomissani komponenteissa.
+*/
 const Header = (props) =>{
   return (
     <div>
