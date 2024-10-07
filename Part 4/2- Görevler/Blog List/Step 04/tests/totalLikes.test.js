08ca2bef5ee756f5e8e31007941eb595d44eac46
Bayram-Erdogan <bayramerdogan.fi@gmail.com>
2024-10-07 08:15:30 +0300

    Step 04

---
+++ Part 4/2- Görevler/Blog List/Step 04/tests/totalLikes.test.js
@@ -0,0 +1,99 @@
+/*  ilk olarak bu class icin gerekli olan modulleri yukluyorum. */
+
+const { describe, test } = require('node:test');    /*  ==> Test yazmak için kullanılan bir modüldür. describe ve test
+                                                            fonksiyonları, testleri organize etmek ve çalıştırmak için
+                                                            kullanılır.
+                                                    */
+const assert = require('node:assert');              /*  ==> Doğrulama yapmak için kullanılan bir modüldür.
+                                                            assert.strictEqual ile iki değerin eşit olup olmadığını
+                                                            kontrol eder.
+                                                    */
+const listHelper = require('../utils/list_helper'); /*  ==> Test edilen yardımcı fonksiyonların bulunduğu modüldür.*/
+
+/*
+    Daha sonra test grubumu taniliyorum ve icerisine const listWithOneBlog (Tek bir blog nesnesi içeren bir dizi. Blog
+    nesnesi,blogun ID'sini, başlığını, yazarını, URL'sini, beğeni sayısını ve versiyon numarasını içerir.) icinde örnek
+    veriyi ekledim.
+*/
+
+describe('total likes', () => {
+    const listWithOneBlog = [
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 5,
+        __v: 0
+      }
+    ]
+/*
+      Son olarak testi tanimlayip calistiracak kodu yazdim.
+*/
+    test('when list has only one blog, equals the likes of that', () => {   /*  ==> test: Tek bir test tanımlar. Bu
+                                                                                    testin açıklaması "when list has
+                                                                                    only one blog, equals the likes
+                                                                                    of that" şeklindedir.
+                                                                            */
+      const result = listHelper.totalLikes(listWithOneBlog)   /*  ==> listHelper modülündeki totalLikesfonksiyonu
+                                                                      çağrılır ve listWithOneBlog parametresi ile
+                                                                      çalıştırılır. Bu fonksiyon, blog listesindeki
+                                                                      toplam beğeni sayısını hesaplar.
+                                                              */
+      assert.strictEqual(result, 5)       /*  ==> result ile 5 strictEqual kullanılarak karşılaştırılır. Yani result
+                                                  değeri 5'e eşit olmalıdır. Eğer bu değerler eşit değilse, test
+                                                  başarısız olur ve hata verir.
+                                          */
+    })
+
+
+    test('of empty list is zero', () => {
+        const emptyList = []
+        const result = listHelper.totalLikes(emptyList)
+        assert.strictEqual(result, 0)
+      })
+
+      test('of a bigger list is calculated right', () => {
+        const listWithMultipleBlogs = [
+            {
+                "_id": "66f12f54eb068cf7bb2551fd",
+                "title": "Introduction to Node.js",
+                "author": "Jane Smith",
+                "url": "https://example.com/intro-to-nodejs",
+                "likes": 78,
+                "__v": 0
+              },
+              {
+                "_id": "66f12f61eb068cf7bb2551ff",
+                "title": "Understanding JavaScript Closures",
+                "author": "John Doe",
+                "url": "https://example.com/javascript-closures",
+                "likes": 42,
+                "__v": 0
+              },
+              {
+                "_id": "66f17f9418e6962226edd589",
+                "title": "To Kill a Mockingbird",
+                "author": "Harper Lee",
+                "url": "",
+                "likes": 86,
+                "__v": 0
+              },
+              {
+                "_id": "66f17fb018e6962226edd58b",
+                "title": "1984",
+                "author": "George Orwell",
+                "url": "",
+                "likes": 103,
+                "__v": 0
+              }
+        ]
+        const result = listHelper.totalLikes(listWithMultipleBlogs)
+        assert.strictEqual(result, 309)
+      })
+
+/*
+    Özetle, bu test listHelper.totalLikes fonksiyonunun doğru çalıştığını ve tek bir blog içeren bir listede toplam
+    beğeni sayısını doğru hesapladığını doğrulamak için yazılmıştır.
+*/
+  })
